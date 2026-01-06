702fff6014ed82209846ecc78755db27a9cd8edf
John Lampa <jklampa31@gmail.com>
2026-01-06 22:45:11 +0800

    feat: set up frontend with vite, react, and tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import NavBar from "./components/NavBar"
+import ProductPage from "./pages/ProductPage"
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-base-200 transition-colors duration-300">
+
+        <NavBar />
+        <Routes >
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App
