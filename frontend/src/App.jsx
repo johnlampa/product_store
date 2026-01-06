34f44c617b4861e605ff6991e3688bb41eb3b842
John Lampa <jklampa31@gmail.com>
2026-01-06 23:44:45 +0800

    feat: added navbar and implemented theme management functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,14 @@ import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import NavBar from "./components/NavBar"
 import ProductPage from "./pages/ProductPage"
+import { useThemeStore } from "./store/useThemeStore";
 
 function App() {
+  const { theme } = useThemeStore();
+
   return (
     <>
-      <div className="min-h-screen bg-base-200 transition-colors duration-300">
+      <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme}>
 
         <NavBar />
         <Routes >
