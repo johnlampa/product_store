f3ac30ad6ff4842fb268e6ce2c2aa917e4c3f6b7
John Lampa <jklampa31@gmail.com>
2026-02-25 12:05:35 +0800

    fix: changed to php sign

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -20,7 +20,7 @@ function ProductCard({product}) {
         <div className="card-body">
             {/* PRODUCT INFO */}
             <h2 className="card-title text-lg font-semibold">{product.name}</h2>
-            <p className="text-2xl font-bold text-primary">${Number(product.price).toFixed(2)}</p>
+            <p className="text-2xl font-bold text-primary">₱{Number(product.price).toFixed(2)}</p>
 
             {/* CARD ACTIONS */}
             <div className="card-actions justify-end mt-4">
